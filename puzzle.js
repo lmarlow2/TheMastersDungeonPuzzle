c4ee982a6ea60ed5040c59bd77cc814ebdcb5a4e
lmarlow2 <47681639+lmarlow2@users.noreply.github.com>
2023-06-18 21:41:28 -0500

    Update puzzle.js
    Added logic to check the puzzle solution.

--- puzzle.js
+++ puzzle.js
@@ -1,1 +1,29 @@
+function calculateSums(){
+  let a, b, c, d, e, f, g, h, i;
+  a = parseInt(document.getElementById("a").value);
+  b = parseInt(document.getElementById("b").value);
+  c = parseInt(document.getElementById("c").value);
+  d = parseInt(document.getElementById("d").value);
+  e = parseInt(document.getElementById("e").value);
+  f = parseInt(document.getElementById("f").value);
+  g = parseInt(document.getElementById("g").value);
+  h = parseInt(document.getElementById("h").value);
+  i = parseInt(document.getElementById("i").value);
+  //rows
+  let abc = a + b + c;
+  let def = d + e + f;
+  let ghi = g + h + i;
+  //cols
+  let adg = a + d + g;
+  let beh = b + e + h;
+  let cfi = c + f + i;
+  //diags
+  let aei = a + e + i;
+  let ceg = c + e + g;
 
+  if(abc == def && def == ghi && ghi == adg && adg == beh && beh == cfi && cfi == aei && aei == ceg && ceg == 15){
+    console.log("Correct Answer Submitted!");
+  } else {
+    console.log("This answer is not correct!");
+  }
+}
